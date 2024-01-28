311f4c01db533701b43fd904cfc8219cace84f71
Vicky <vicky@cartoonmango.com>
2024-01-28 20:44:09 +0530

    added error handling

--- sender.py
+++ sender.py
@@ -1,30 +1,37 @@
-import pika, json, os
+import pika
+import json
+import os
+import sys
 from dotenv import load_dotenv
 
 # Get the .env variables
 load_dotenv()
 
-# rabbitMQ connection
-connection = pika.BlockingConnection(pika.ConnectionParameters(os.getenv('RABBITMQ_HOST')))
+try:
+    # rabbitMQ connection
+    with pika.BlockingConnection(pika.ConnectionParameters(os.getenv('RABBITMQ_HOST'))) as connection:
 
-# open connection 
-channel = connection.channel()
+        # open channel 
+        channel = connection.channel()
 
-queue_name = os.getenv('RABBITMQ_QUEUE_NAME')
+        queue_name = os.getenv('RABBITMQ_QUEUE_NAME')
 
-# creating a queue
-channel.queue_declare(queue=queue_name)
+        # creating/declaring a queue
+        channel.queue_declare(queue=queue_name)
 
-file_path = os.getenv('JSON_FILE_PATH')
+        file_path = os.getenv('JSON_FILE_PATH')
 
-# read the file
-with open(file_path, "r") as file:
-    data = json.load(file)
+        # read the file
+        with open(file_path, "r") as file:
+            data = json.load(file)
 
-for json_data in data:
-    # Publish a message in the queue
-    channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(json_data))
-    print(" [{}] Sent {}".format(queue_name, json_data))
+        for json_data in data:
+            # Publish a message in the queue
+            channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(json_data))
+            print(" [{}] Sent {}".format(queue_name, json_data))
 
-# close connection
-connection.close()
+        print("Done.")
+
+except Exception as e:
+    print(f"Error: {e}")
+    sys.exit(1)
